7e85498e6778bd1a85349994c867e9e24dabff32
Abraão <abraao.neto@souunit.com.br>
2025-10-27 13:38:50 -0300

    1

---
+++ src/main/java/br/com/grupo5/AgendaEventos/Projeto_de_Programa_o_N04_Grupo5/ProjetoDeProgramaON04Grupo5Application.java
@@ -0,0 +1,13 @@
+package br.com.grupo5.AgendaEventos.Projeto_de_Programa_o_N04_Grupo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeProgramaON04Grupo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeProgramaON04Grupo5Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/grupo5/AgendaEventos/Projeto_de_Programa_o_N04_Grupo5/ProjetoDeProgramaON04Grupo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupo5.AgendaEventos.Projeto_de_Programa_o_N04_Grupo5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeProgramaON04Grupo5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
